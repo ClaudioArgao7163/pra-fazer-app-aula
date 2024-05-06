6db091017217c3ebff200859353f5b9882e31a23
Claudio Roberto França de Aragão <221.14.040@EDU.gruposete.local>
2024-05-06 20:22:56 -0300

    implementaçao do firebase

--- src/screens/CreateUser/index.js
+++ src/screens/CreateUser/index.js
@@ -1,24 +1,45 @@
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
+import { firebase } from '../../services/firebaseConfig'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 import styles from './style'
 
-export default function CreateUser() {
-    
+export default function CreateUser({navigation}) {
+
     const [nome, setNome] = useState("")
     const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
-    const [errorCreateUser, setErrorCreateUser] = useState (null)
+    const [password, setPassword] = useState("")
+    const [errorCreateUser, setErrorCreateUser] = useState(null)
 
-    function Validate (){
-        if (nome == ""){
+    function Validate() {
+        if (nome == "") {
             setErrorCreateUser("informe o seu nome")
         } else if (email == "") {
             setErrorCreateUser("Informe o seu e-mail")
-        } else if (senha == "") {
+        } else if (password == "") {
             setErrorCreateUser("Informe uma senha");
         } else {
             setErrorCreateUser(null)
+            CreateUser();
         }
+    }
+
+    const CreateUser = () => {
+
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+            
+                const user = userCredential.user;
+                navigation.navigate('Tabs')
+        
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setErrorCreateUser(errorMessage);
+      
+            });
     }
 
     return (
@@ -44,9 +65,9 @@ export default function CreateUser() {
             <TextInput
                 style={styles.input}
                 secureTextEntry={true}
-                placeholder='Senha'
-                value={senha}
-                onChangeText={setSenha}
+                placeholder='password'
+                value={password}
+                onChangeText={setPassword}
             />
 
             <TouchableOpacity
---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKBxhB6612tTPGif0otHbosjLceR2ePv4",
+  authDomain: "pra-fazer-app-40a6b.firebaseapp.com",
+  projectId: "pra-fazer-app-40a6b",
+  storageBucket: "pra-fazer-app-40a6b.appspot.com",
+  messagingSenderId: "99200336487",
+  appId: "1:99200336487:web:f27faf2a91e509ef57cac2"
+};
+
+const app = initializeApp(firebaseConfig);
